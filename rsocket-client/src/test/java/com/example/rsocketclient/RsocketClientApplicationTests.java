7a080aa266cd4531993f898383a2a6b2080345a7
Rob Winch <rwinch@users.noreply.github.com>
2019-12-11 13:38:00 -0600

    Add Test

--- rsocket-client/src/test/java/com/example/rsocketclient/RsocketClientApplicationTests.java
+++ rsocket-client/src/test/java/com/example/rsocketclient/RsocketClientApplicationTests.java
@@ -2,12 +2,23 @@ package com.example.rsocketclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RsocketClientApplicationTests {
 
   @Test
-  void contextLoads() {
+  void works(RSocketRequester.Builder builder) {
+    RSocketRequester localhost = builder
+            .connectTcp("localhost", 8888)
+            .block();
+    GreetingResponse response = localhost
+            .route("greeting")
+            .retrieveMono(GreetingResponse.class)
+            .block();
+    assertThat(response.getMessage()).isEqualTo("greeting");
   }
 
 }
